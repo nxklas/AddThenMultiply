e7c6a4970e93487682eaf00bdd6af4b812ef4491
nxklas <niklas-bayer@gmx.de>
2023-07-01 03:08:06 +0200

    Added a JavaScript implementation of AddThenMultiply.

---
+++ js/AddThenMultiply.js
@@ -0,0 +1,18 @@
+function atm(start1, start2, threshold) {
+  if (threshold == 0)
+    return start1
+
+  var isEven = false
+
+  for (var i = 0; i < threshold; i++)
+  {
+    isEven = i % 2 == 0
+
+    if (isEven)
+      start1 += start2
+    else
+      start2 *= start1
+  }
+
+  return isEven ? start1 : start2
+}
